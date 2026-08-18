5d1dc87ef64130647faf328f5bcadfc3a8436eef
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-08-18 09:30:30 +0530

    Promote release candidate after compositor hardening

--- app/version.py
+++ app/version.py
@@ -1,6 +1,6 @@
 """Live Pookalam release identity."""
 
-VERSION = "1.0.0-rc1"
-RELEASE_NAME = "Field Release Candidate 1"
+VERSION = "1.0.0-rc2"
+RELEASE_NAME = "Field Release Candidate 2"
 PRODUCT = "LIVE POOKALAM"
 AUTHOR = "bnsh.eb"
